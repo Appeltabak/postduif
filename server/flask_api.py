0e667b09165f74cc802f4e9f19c0b5b5b60f5365
Tonnis Oosterhoff <tonnisoosterhoff@gmail.com>
2015-02-06 14:30:19 +0100

    Updated .gitignore; Updated flask api, now able to start with path instead of first browsing to directory

--- server/flask_api.py
+++ server/flask_api.py
@@ -4,6 +4,13 @@ from flask import jsonify
 from decorator import crossdomain
 app = Flask(__name__)
 
+import os
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE_URI = os.path.join(_basedir, 'database.db')
+
+
 def dict_factory(cursor, row):
     """Formats sqlite3 output to JSON"""
     d = {}
@@ -11,16 +18,17 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+
 @app.route("/api/get_flight/<id>", methods=['GET', 'OPTIONS'])
 @crossdomain(origin='*')
 def get_flight(id):
-	"""Fetches flight data from sqlite3 based on flight id
-	Args:
-		id: the id of the flight.
+    """Fetches flight data from sqlite3 based on flight id
+    Args:
+        id: the id of the flight.
 
-	Returns:
-		One JSON formated data object of a single flight.
-		example:
+    Returns:
+        One JSON formated data object of a single flight.
+        example:
         {
           "begin_lat": 53.232787,
           "begin_long": 6.5709585,
@@ -30,14 +38,15 @@ def get_flight(id):
           "speed": 22,
           "start_time": 1422917121
         }
-	"""
-	con = sqlite3.connect("database.db") #relative path to database file.
-	con.row_factory = dict_factory
-	cur = con.cursor()
-	cur.execute("SELECT * FROM flight WHERE id = " + str(id))
-	result = cur.fetchone()
-	con.close()
-	return jsonify(result)
+    """
+    # relative path to database file.
+    con = sqlite3.connect(DATABASE_URI)
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("SELECT * FROM flight WHERE id = " + str(id))
+    result = cur.fetchone()
+    con.close()
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
