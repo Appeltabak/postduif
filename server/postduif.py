d57fd0c21e1f87016c42cdf2ac7a0e455e29a3db
Tonnis Oosterhoff <tonnisoosterhoff@gmail.com>
2015-02-10 22:44:47 +0100

    Added Message and User mapping.

--- server/postduif.py
+++ server/postduif.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, Enum, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Enum, ForeignKey, \
+    Text, Boolean
 from sqlalchemy.orm import sessionmaker, relationship
 
 engine = create_engine('sqlite:///database.db', echo=True)
@@ -31,4 +32,25 @@ class Flight(Base):
     start_time = Column(Integer)
     end_time = Column(Integer)
     msg_id = Column(ForeignKey('msg_id'))
-    
+
+
+class Message(Base):
+    __tablename__ = 'message'
+
+    id = Column(Integer, primary_key=True)
+    msg = Column(Text)
+    receiver_id = Column(ForeignKey('user_id'))
+    sender_id = Column(ForeignKey('user_id'))
+    sealed = Column(Boolean)
+    status = Column(Integer)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    password = Column(String(50))
+    salt = Column(String(50))
+    email = Column(String(50))
+    loc = Column(String(100))
